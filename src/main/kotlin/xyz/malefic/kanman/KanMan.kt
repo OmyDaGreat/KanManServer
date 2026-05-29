df2e1d8abfb5593e30f571d78813502d67538802
Om Gupta <ogupta4242@gmail.com>
2026-05-28 21:25:38 -0700

    feat(routes): add user registration

--- src/main/kotlin/xyz/malefic/kanman/KanMan.kt
+++ src/main/kotlin/xyz/malefic/kanman/KanMan.kt
@@ -6,6 +6,7 @@ import org.http4k.core.Method.GET
 import org.http4k.core.Method.POST
 import org.http4k.core.Method.PUT
 import org.http4k.core.Response
+import org.http4k.core.Status
 import org.http4k.core.Status.Companion.OK
 import org.http4k.core.then
 import org.http4k.filter.AllowAllOriginPolicy
@@ -16,6 +17,10 @@ import org.http4k.routing.bind
 import org.http4k.routing.routes
 import org.http4k.server.Undertow
 import org.http4k.server.asServer
+import org.jetbrains.exposed.v1.core.eq
+import org.jetbrains.exposed.v1.jdbc.Database
+import org.jetbrains.exposed.v1.jdbc.SchemaUtils
+import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 
 fun createApp(): HttpHandler {
     val corsPolicy =
@@ -31,9 +36,25 @@ fun createApp(): HttpHandler {
         routes(
             "/api/ping" bind GET to { Response(OK).body("pong") },
             "/api/health" bind GET to { Response(OK).body("healthy") },
-            "/api/board/{id}" bind GET to { request ->
-                val id = request.query("id")
-                Response(OK).body("id: $id") // TODO: Return board contents // TODO: Figure out auth w/ visibility
+            "/api/user/register" bind POST to POST@{ request ->
+                val username =
+                    request.query("username") ?: return@POST Response(Status.BAD_REQUEST).body("Expected username, instead got nothing")
+                val password =
+                    request.query("password") ?: return@POST Response(Status.BAD_REQUEST).body("Expected password, instead got nothing")
+
+                // TODO: More proper user creds validation (+ password hashing)
+
+                try {
+                    transaction {
+                        UserEntity.new {
+                            this.username = username
+                            this.password = password
+                        }
+                    }
+                } catch (e: Exception) {
+                    return@POST Response(Status.BAD_REQUEST).body("Failed to create user: $e")
+                }
+                Response(OK).body("User added with username $username and password $password")
             },
         ),
     )
@@ -42,6 +63,16 @@ fun createApp(): HttpHandler {
 val app: HttpHandler by lazy { createApp() }
 
 fun main() {
+    Database.connect(
+        url = "jdbc:sqlite:data.db", // TODO: Actually decide file name
+        driver = "org.sqlite.JDBC",
+    )
+
+    transaction {
+        exec("PRAGMA foreign_keys = ON;")
+        SchemaUtils.create(Users, Boards, StickyNotes, BoardUsers)
+    }
+
     val printingApp: HttpHandler = PrintRequest().then(app)
 
     val server = printingApp.asServer(Undertow(6320)).start()
